b582ce4dc67b97f17ed9ce2713c2aeb9e1365e8c
Yanbing Fang <yanbingfang@utexas.edu>
2020-07-07 20:15:56 -0500

    Add files via upload

--- wdb/main.js
+++ wdb/main.js
@@ -1,12 +1,16 @@
 var express = require("express"),
+	methodOverride = require("method-override"),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
-	app = express()
+	app = express(),
+	expressSanitizer = require("express-sanitizer")
 
 mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer()); //sanitize input so that user wouldn't input html and interrupt with our page
 
 // //MONGOOSE/ MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -21,6 +25,8 @@ var Blog = mongoose.model("Blog",blogSchema);
 app.get("/",function(req,res){
 	res.redirect("/blogs");
 })
+
+//INDEX ROUTE
 app.get("/blogs",function(req,res){
 	Blog.find({},function(err,blogs){
 		if(err){
@@ -31,6 +37,67 @@ app.get("/blogs",function(req,res){
 	})
 })
 
+//NEW ROUTE
+app.get("/blogs/new",function(req,res){
+	res.render("new");
+})
+
+//CREATE ROUTE
+app.post("/blogs",function(req,res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);//remove input stuff like a script tag
+	Blog.create(req.body.blog,function(err,newBlog){
+		if(err){
+			res.render("new");
+		}else{
+			res.redirect("/blogs");
+		}
+	})
+})
+
+//SHOW ROUTE
+app.get("/blogs/:id",function(req,res){
+	Blog.findById(req.params.id,function(err,foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("show",{blog:foundBlog});
+		}
+	})
+})
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit",function(req,res){
+	Blog.findById(req.params.id,function(err,foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("edit",{blog:foundBlog});
+		}
+	})
+})
+
+//UPDATE ROUTE
+app.put("/blogs/:id",function(req,res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs/" + req.params.id);
+		}
+	})
+})
+
+//DELETE ROUTE
+app.delete("/blogs/:id",function(req,res){
+	Blog.findByIdAndRemove(req.params.id,function(err){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs");
+		}
+	})
+})
 
 app.listen(3000, function(){
 	console.log("SERVER is RUNNING");
